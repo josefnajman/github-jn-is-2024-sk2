ce3e451aa53d00bf46d477ae72dfb254c1b9f85a
Josef Najman <josef.najman@tul.cz>
2024-10-14 15:44:26 +0200

    #1 zakladni program s moznosti opakovani

---
+++ IS-Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Specialized;
+class Program {
+    static void Main() {
+        // Chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+            //Console.Clear();
+            Console.WriteLine("**************************");
+            Console.WriteLine("******* Výpis řady *******");
+            Console.WriteLine("**************************");
+            Console.WriteLine("****** Josef Najman ******");
+            Console.WriteLine("**************************\n\n");
+            Console.WriteLine();
+            // Vstup od uživatele - TO-DO
+            // Logika pro výpis řady - TO-DO
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+    }   
+}
+
